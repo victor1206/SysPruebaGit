883d5773fd2526fbd00cb941b114708aba53c322
victor1206 <victor_rivas89@hotmail.com>
2024-02-13 14:59:49 -0600

    Se agrego clases Categoria y Producto

---
+++ SysPruebaGit.EN/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysPruebaGit.EN
+{
+    internal class Producto
+    {
+        public int Id { get; set; }
+        public int IdCategoria { get; set; }
+        public string Nombre { get; set; }
+    }
+}
